a54a14439f486e247d4058c7d1f4b758788a1c35
Mostafa-K-Fakhry <mostafa856123@gmail.com>
2026-08-17 23:25:26 +0300

    chore: prepare project for deployment

--- index.js
+++ index.js
@@ -4,30 +4,64 @@ import locationRoutes from "./routes/location.route.js";
 import episodeRoutes from "./routes/episode.route.js";
 import dotenv from "dotenv";
 
-dotenv.config({ path: "./config.env" });
+dotenv.config();
+
 const app = express();
 
 app.use(express.json());
+
 app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 
+
+// Routes
+
 app.use("/", characterRoutes);
+
 app.use("/", locationRoutes);
+
 app.use("/", episodeRoutes);
+
+
+// Default Route
+
 app.get("/", (req, res) => {
     res.redirect("/characters");
 });
+
+
+// 404
+
 app.use((req, res) => {
     res.status(404).render("pages/404");
 });
+
+
+// Global Error Handler
+
 app.use((err, req, res, next) => {
+
     console.error(err);
 
     res.status(500).render("pages/500");
+
 });
 
-const port = process.env.PORT;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// Local Development
+
+if (!process.env.VERCEL) {
+
+    const port = process.env.PORT || 3000;
+
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+
+}
+
+
+// Export for Vercel
+
+export default app;
